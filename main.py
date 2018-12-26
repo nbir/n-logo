ac1b037e0cd61ab3906eb15ea97019ca90c464ae
Nibir Bora <nibirbora@gmail.com>
2018-12-25 22:39:30 -0800

    Automatically center logo in given width/height

--- main.py
+++ main.py
@@ -6,34 +6,58 @@ PERIOD_VERTICES = [(8, 4), (9, 4), (9, 6), (8, 6)]
 
 WIDTH = 9
 HEIGHT = 6
-PADDING = 1
-SCALE = 100
-
-BACKGROUND_COLOR = 'black'
-FOREGROUND_COLOR = 'white'
+ASPECT_RATIO = WIDTH / HEIGHT
 
 
-def translate_vertices(vertices):
-    return list(map(lambda v: (v[0] + PADDING, v[1] + PADDING), vertices))
+def get_target_dimensions(width, height, padding):
+    actual_width = width - padding
+    actual_height = height - padding
+    aspect_ratio = width / height
+
+    target_width = actual_width
+    target_height = actual_height
+
+    if aspect_ratio < ASPECT_RATIO:
+        target_height = actual_width / ASPECT_RATIO
+    else:
+        target_width = actual_height * ASPECT_RATIO
+
+    return (target_width, target_height)
 
 
-def scale_vertices(vertices):
-    return list(map(lambda v: (v[0] * SCALE, v[1] * SCALE), vertices))
+def scale_vertices(vertices, scale):
+    return list(map(lambda v: (v[0] * scale, v[1] * scale), vertices))
 
 
-def main():
-    width = (WIDTH + 2 * PADDING) * SCALE
-    height = (HEIGHT + 2 * PADDING) * SCALE
-    img = Image.new('1', (width, height), color=BACKGROUND_COLOR)
+def get_transform_params(width, height, target_width, target_height):
+    x_offset = (width - target_width) / 2
+    y_offset = (height - target_height) / 2
+
+    return (1, 0, -x_offset, 0, 1, -y_offset)
+
+
+def generate(width, height, padding=0, bgcolor='black'):
+    (target_width, target_height) = get_target_dimensions(
+        width, height, padding)
+
+    scale = target_width / WIDTH
+
+    letter_vertices = scale_vertices(LETTER_VERTICES, scale)
+    period_vertices = scale_vertices(PERIOD_VERTICES, scale)
+
+    img = Image.new('1', (width, height), color=bgcolor)
 
     draw = ImageDraw.Draw(img)
-    letter_vertices = scale_vertices(translate_vertices(LETTER_VERTICES))
-    period_vertices = scale_vertices(translate_vertices(PERIOD_VERTICES))
     draw.polygon(letter_vertices, fill=FOREGROUND_COLOR)
     draw.polygon(period_vertices, fill=FOREGROUND_COLOR)
+
+    transform_params = get_transform_params(width, height, target_width,
+                                            target_height)
+
+    img = img.transform(img.size, Image.AFFINE, transform_params)
 
     img.show('N Logo')
 
 
 if __name__ == '__main__':
-    main()
+    generate(460, 460, 50)  # TODO: use argparse
